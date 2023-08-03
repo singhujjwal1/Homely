1df1f769906f53efb1d082b7d5604d3422d765d9
Aman Kumar <029amankumar@gmail.com>
2023-08-04 02:45:23 +0530

    deploy

--- api/index.js
+++ api/index.js
@@ -26,7 +26,7 @@ app.use("/uploads", express.static(__dirname + "/uploads"));
 app.use(
   cors({
     credentials: true,
-    origin: "https://homely-5vxy.onrender.com/",
+    origin: "https://homely-5vxy.onrender.com",
   })
 );
 
